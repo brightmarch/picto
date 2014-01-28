b08f27a4b128f7ed52e21d1f8ae8f1f44aacee91
Vic Cherubini <vcherubini@gmail.com>
2014-01-28 14:18:21 -0600

    Now deploys to apps.brightmarch.com.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-role :web, 'picto.brightmarch.com'
-role :db,  'picto.brightmarch.com', :primary => true
+role :web, 'apps.brightmarch.com'
+role :db,  'apps.brightmarch.com', :primary => true
 
 set :deploy_to, '/srv/http/apps/picto'
